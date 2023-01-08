ca12330a5f341c9eb00c9baee399bff646575d87
ruddnev <nikita-rudnev@ya,ru>
2023-01-08 22:17:49 +0300

    make user choose file extension

--- readme-generator/generator.py
+++ readme-generator/generator.py
@@ -6,13 +6,19 @@ import re
 if __name__ == '__main__':
 
     if len(sys.argv) == 1:
+        # НАЧАЛО ЧАСТИ, КОТОРУЮ НУЖНО ИЗМЕНИТЬ
+
         githubName = "ваш github ник"
         repoName = "название вашего репозитория"
         header = "заголовок вашего сайта"
-    elif len(sys.argv) == 4:
+        fileExtension = ".jpg"
+
+        # КОНЕЦ ЧАСТИ, КОТОРУЮ НУЖНО ИЗМЕНИТЬ
+    elif len(sys.argv) == 5:
         githubName = sys.argv[1]
         repoName = sys.argv[2]
         header = sys.argv[3]
+        fileExtension = sys.argv[4]
     else:
         print("Неверное количество аргументов")
         sys.exit()
@@ -28,6 +34,7 @@ if __name__ == '__main__':
                 print("Нашел пустую строку в cardsList.txt")
             else:
                 cards.append(line.rstrip())
+
     if re.search(r'^[0-9]+\.', cards[1]):
         if cards[1][0] != "1":
             print("Неправильный список билетов в cardsList.txt! Уберите нумерацию")
@@ -36,33 +43,8 @@ if __name__ == '__main__':
         if re.search(r'^[0-9]+\.', cards[i]):
             cards[i] = re.search(r"(?<=\. ).*", cards[i]).group()
 
-
-    mainFiles = os.listdir("../cards")
-    altFiles = os.listdir("../altCards")
-
-    mainExt = [".none"] * len(cards)
-    altExt = [".none"] * len(cards)
-
-    countGood = 0
-    for file in mainFiles:
-        n = False
-        n = int(re.match(r"^[0-9]+(?=\.(jpg|png|jpeg))", file).group())
-        if n:
-            mainExt[n] = re.search(r"\.(jpg|png|jpeg)", file).group()
-            countGood += 1
-
-    if (len(cards) - 1) != countGood:
-        print("Количество билетов не сходится с количеством картинок! Поправьте, или будут пустые билеты")
-
-    for file in altFiles:
-        n = False
-        n = int(re.search(r"^[0-9]+(?=\.(jpg|png|jpeg))", file).group())
-        if n:
-            altExt[n] = re.search(r"\.(jpg|png|jpeg)", file).group()
-
     if writeToReadme:
         readme = open("../README.md", "w", encoding='utf-8')
-
 
     def printWrite(*args):
         line = ' '.join([str(arg) for arg in args])
@@ -109,12 +91,10 @@ if __name__ == '__main__':
 
     # Формируем сами билеты
     for n in range(1, howManyCards + 1):
-
-
         printWrite("##", cards[n], "[" + str(n) + "] [наверх](" + linkToTop + ")")
-        printWrite("![" + cards[n] + "](" + imageLinkTemplate + str(n) + mainExt[n] + ")")
+        printWrite("![" + cards[n] + "](" + imageLinkTemplate + str(n) + fileExtension + ")")
         if n in alts:
             printWrite("### Альтернативный билет для ", cards[n], "[" + str(n) + "] [наверх](" + linkToTop + ")")
-            printWrite("![" + cards[n] + "](" + altsLink + str(n) + altExt[n] + ")")
+            printWrite("![" + cards[n] + "](" + altsLink + str(n) + fileExtension + ")")
         printWrite("---")
 
